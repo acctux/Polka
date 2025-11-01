96820851021c77a5378047a5790eecf8ab21c1b9
Nick A <226946345+acctux@users.noreply.github.com>
2025-10-31 21:11:03 -0400

    split

---
+++ local/bin/logid/personal_logid.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+DEVICE="MX Master 3S"
+LOG="$HOME/.cache/logid_check.log"
+ATTEMPTS=10
+LOGID_DELAY=3
+
+LOGID_PID=0
+restart_logid() {
+  sudo logid restart >"$LOG" 2>&1 &
+  LOGID_PID=$!
+  echo "$LOGID_PID" >>"$LOG"
+  sleep "$LOGID_DELAY"
+}
+
+run_attempts() {
+  for i in $(seq 1 "$ATTEMPTS"); do
+    if grep -q "${DEVICE}" "${LOG}"; then
+      if ! grep -q "disconnected" "${LOG}"; then
+        return 0
+      fi
+    else
+      restart_logid
+    fi
+  done
+}
+
+main() {
+  rm "${LOG}"
+  if run_attempts; then
+    echo "Connected with $LOGID_PID" >>"$LOG"
+  else
+    LOGID_DELAY=10
+    ATTEMPTS=20
+    run_attempts
+  fi
+}
+main
