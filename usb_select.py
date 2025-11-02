c7b79931e06157c6a7fe3a2d1d54eaf4fdddae2e
Nick A <226946345+acctux@users.noreply.github.com>
2025-11-02 01:15:13 -0400

    p

--- usb_select.py
+++ usb_select.py
@@ -4,12 +4,35 @@ import subprocess
 import os
 import shutil
 from pathlib import Path
-from util.utils import log
+import logging
 
 # -------------------- Constants -------------------- #
 USB_FS_TYPE = "exfat"
 MIN_SIZE = "20G"
 USB_MNT = "/mnt/usb"
+
+
+COLORS = {
+    "INFO": "\033[36m",  # cyan
+    "SUCCESS": "\033[32m",  # green
+    "WARNING": "\033[33m",  # yellow
+    "ERROR": "\033[31m",  # red
+    "RESET": "\033[0m",
+}
+
+
+class ColorFormatter(logging.Formatter):
+    def format(self, record):
+        color = COLORS.get(record.levelname, COLORS["RESET"])
+        message = super().format(record)
+        return f"{color}{message}{COLORS['RESET']}"
+
+
+log = logging.getLogger("keysync")
+log.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(ColorFormatter("[%(levelname)s] %(message)s"))
+log.addHandler(handler)
 
 
 # -------------------- Utilities -------------------- #
