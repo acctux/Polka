191d4ffa4ae99454d3010c67f94de5c266f4e11e
Nick A <226946345+acctux@users.noreply.github.com>
2025-12-23 00:50:35 +0100

    p

--- local/bin/tz.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-ZONEINFO=/usr/share/zoneinfo
-ZONETAB="$ZONEINFO/zone1970.tab"
-CURRENT_TZ=$(timedatectl show --property=Timezone --value)
-TZONE=$(awk 'NF && $1 !~ /^#/ {print $3}' "$ZONETAB" | sort | fuzzel --dmenu --prompt="Timezone ($CURRENT_TZ): ")
-[ -z "$TZONE" ] && exit 0
-alacritty -e sudo timedatectl set-timezone "$TZONE"
