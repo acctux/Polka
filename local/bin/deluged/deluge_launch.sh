1d141ef52a0ef3e2e0ecb6633674a0407901d3c0
Nick A <226946345+acctux@users.noreply.github.com>
2026-01-01 22:13:42 -0500

    p

--- local/bin/deluged/deluge_launch.sh
+++ local/bin/deluged/deluge_launch.sh
@@ -4,7 +4,7 @@ set -e
 systemctl --user start deluged.service
 for _ in {1..30}; do
   if deluge-console info >/dev/null 2>&1; then
-    exec deluge-gtk
+    exec deluge-gtk "$@"
   fi
   sleep 2
 done
